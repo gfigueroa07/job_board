418edf179e8cf2990bbd021448bb5c1100d66e57
gfigueroa07 <guillermofigueroa2840@gmail.com>
2025-09-15 20:26:53 -0400

    partially created profile db model

--- job-project/job_board/users/models.py
+++ job-project/job_board/users/models.py
@@ -8,4 +8,5 @@ class Profile(models.Model):
     email = models.EmailField(blank=True, null=True)
     phone = models.CharField(max_length=20, blank=True, null=True)
 
-    
+class Employer(models.Model):
+    pass
