89c42134f018d557dbee15200fa02684af36a490
gfigueroa07 <guillermofigueroa2840@gmail.com>
2026-04-17 23:50:23 -0400

    feat: update DATABASES setting to use environment variable for database URL

--- job_board/settings.py
+++ job_board/settings.py
@@ -83,11 +83,9 @@ WSGI_APPLICATION = 'job_board.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
+    )
 }
 
 
