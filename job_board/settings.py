63089b5f5f82e7b5bca8d9b709dfbfba465d1756
gfigueroa07 <guillermofigueroa2840@gmail.com>
2026-04-19 01:12:35 -0400

    feat: update DATABASES setting to support local development and production environments

--- job_board/settings.py
+++ job_board/settings.py
@@ -85,13 +85,24 @@ WSGI_APPLICATION = 'job_board.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600
-    )
-}
+BASE_DIR = Path(__file__).resolve().parent.parent
 
+if os.environ.get("DATABASE_URL"):
+    # Production (Render)
+    DATABASES = {
+        "default": dj_database_url.parse(
+            os.environ.get("DATABASE_URL"),
+            conn_max_age=600
+        )
+    }
+else:
+    # Local development
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
