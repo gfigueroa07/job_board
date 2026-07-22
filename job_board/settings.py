7bad276cd003bda1d4edd45b66780206c172235c
gfigueroa07 <guillermofigueroa2840@gmail.com>
2026-07-21 22:13:27 -0400

    Add SSL requirement to database connection settings

--- job_board/settings.py
+++ job_board/settings.py
@@ -43,7 +43,8 @@ if DATABASE_URL:
     DATABASES = {
         "default": dj_database_url.parse(
             DATABASE_URL,
-            conn_max_age=600
+            conn_max_age=600,
+            ssl_require=True
         )
     }
 else:
